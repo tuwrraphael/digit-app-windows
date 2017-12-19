5de4ed8d749a705440513facfde37f3e307421c7
Raphael <raphael.hauk@racing.tuwien.ac.at>
2017-12-19 02:33:33 +0100

    send time via ble

---
+++ digit-app/BLE/CTSConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digit_app.BLE
+{
+    public class CTSConverter
+    {
+        public byte[] Convert(DateTime dateTime)
+        {
+            return new byte[] {
+                (byte)(dateTime.Year & 0xFF),
+                (byte)((dateTime.Year >> 8) & 0xFF),
+                (byte)(dateTime.Month & 0xFF),
+                (byte)(dateTime.Day & 0xFF),
+                (byte)(dateTime.Hour & 0xFF),
+                (byte)(dateTime.Minute & 0xFF),
+                (byte)(dateTime.Second & 0xFF),
+                Convert(dateTime.DayOfWeek),
+                (byte)(((float)dateTime.Millisecond / 1000.0) * 256)
+            };
+        }
+
+        private byte Convert(DayOfWeek dayOfWeek)
+        {
+            switch(dayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    return 1;
+                case DayOfWeek.Tuesday:
+                    return 2;
+                case DayOfWeek.Wednesday:
+                    return 3;
+                case DayOfWeek.Thursday:
+                    return 4;
+                case DayOfWeek.Friday:
+                    return 5;
+                case DayOfWeek.Saturday:
+                    return 6;
+                case DayOfWeek.Sunday:
+                    return 7;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- digit-app/MainPage.xaml.cs
+++ digit-app/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +24,53 @@ namespace digit_app
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private string deviceId;
+
         public MainPage()
         {
             this.InitializeComponent();
+            StartBleDeviceWatcher();
+        }
+
+        
+
+        private void StartBleDeviceWatcher()
+        {
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected", "System.Devices.Aep.Bluetooth.Le.IsConnectable" };
+            string aqsAllBluetoothLEDevices = "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")";
+            var deviceWatcher = DeviceInformation.CreateWatcher(aqsAllBluetoothLEDevices,
+                        requestedProperties,
+                        DeviceInformationKind.AssociationEndpoint);
+            deviceWatcher.Added += async (sender, args) =>
+            {
+                if (args.Name == "Digit")
+                {
+                    deviceWatcher.Stop();
+                    try
+                    {
+                        deviceId = args.Id;
+                        var bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(args.Id);
+                        var svcs = (await bluetoothLeDevice.GetGattServicesAsync()).Services
+                        .Where(v => v.Uuid.Equals(new Guid("00001523-1212-efde-1523-785fef13d123"))).FirstOrDefault();
+                        if (null != svcs)
+                        {
+                            byte[] data = new BLE.CTSConverter().Convert(DateTime.Now);
+                            await svcs.GetAllCharacteristics().First().WriteValueAsync(data.AsBuffer());
+                        }
+                        //sendTime();
+                    }
+                    catch (Exception ex) when ((uint)ex.HResult == 0x800710df)
+                    {
+                        // ERROR_DEVICE_NOT_AVAILABLE because the Bluetooth radio is not on.
+                        //Log("Radio not turned on");
+                    }
+                }
+            };
+            //deviceWatcher.Updated += DeviceWatcher_Updated;
+            //deviceWatcher.Removed += DeviceWatcher_Removed;
+            //deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            //deviceWatcher.Stopped += DeviceWatcher_Stopped;
+            deviceWatcher.Start();
         }
     }
 }
