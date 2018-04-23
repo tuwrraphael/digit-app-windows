af74b57736e271235f5dd1dafd32758783fd35ff
Raphael <raphael.hauk@racing.tuwien.ac.at>
2018-04-23 12:25:09 +0200

    protect shared tokens with semaphores

--- DigitAppCore/OpenIdApiClient.cs
+++ DigitAppCore/OpenIdApiClient.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Security.Authentication.Web;
@@ -16,10 +17,6 @@ namespace DigitAppCore
     {
         private readonly string authorizationUrl;
         private readonly string name;
-        private readonly ApplicationDataContainer localSettings;
-        private string accessToken;
-        private string refreshToken;
-        private DateTime? accessTokenExpiration;
         private readonly string clientId;
         private readonly string clientSecret;
         private readonly string scopes;
@@ -40,15 +37,50 @@ namespace DigitAppCore
             this.clientSecret = clientSecret;
             this.scopes = scopes;
             this.redirectUri = redirectUri;
-            localSettings = ApplicationData.Current.LocalSettings;
-            accessToken = (localSettings.Values[AccessTokenKey] as string);
-            refreshToken = (localSettings.Values[RefreshTokenKey] as string);
-            if (localSettings.Values[AccessTokenExpiresKey] is string expiration
-                && DateTime.TryParse(expiration, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime res))
+
+        }
+
+        private class StoredTokens
+        {
+            public DateTime? AccessTokenExpires { get; set; }
+            public string AccessToken { get; set; }
+            public string RefreshToken { get; set; }
+
+            public bool Expired => null == AccessToken || !AccessTokenExpires.HasValue || AccessTokenExpires.Value < DateTime.Now;
+            public bool HasRefreshToken => null != RefreshToken;
+        }
+
+        private static SemaphoreSlim accessTokenSem = new SemaphoreSlim(1);
+
+        private StoredTokens Tokens
+        {
+            get
             {
-                accessTokenExpiration = res;
+                var localSettings = ApplicationData.Current.LocalSettings;
+                var tokens = new StoredTokens()
+                {
+                    AccessToken = (localSettings.Values[AccessTokenKey] as string),
+                    RefreshToken = (localSettings.Values[RefreshTokenKey] as string)
+                };
+                if (localSettings.Values[AccessTokenExpiresKey] is string expiration
+                    && DateTime.TryParse(expiration, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime res))
+                {
+                    tokens.AccessTokenExpires = res;
+                }
+                return tokens;
+            }
+            set
+            {
+                var localSettings = ApplicationData.Current.LocalSettings;
+                localSettings.Values[AccessTokenKey] = value.AccessToken;
+                if (value.AccessTokenExpires.HasValue)
+                {
+                    localSettings.Values[AccessTokenExpiresKey] = value.AccessTokenExpires.Value.ToString(CultureInfo.InvariantCulture);
+                }
+                localSettings.Values[RefreshTokenKey] = value.RefreshToken;
             }
         }
+
 
         public HttpClient DefaultClient()
         {
@@ -61,92 +93,114 @@ namespace DigitAppCore
         public async Task<HttpClient> GetAuthorizedClient()
         {
             var client = DefaultClient();
-            if (null != accessToken && accessTokenExpiration.HasValue && accessTokenExpiration.Value > DateTime.Now)
+            await accessTokenSem.WaitAsync();
+            var tokens = Tokens;
+            if (!tokens.Expired)
             {
-                client.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", accessToken);
+                client.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", tokens.AccessToken);
+                accessTokenSem.Release();
                 return client;
             }
-            else if (null != refreshToken)
+            else if (tokens.HasRefreshToken)
             {
-                var dict = new Dictionary<string, string>
+                try
+                {
+                    var dict = new Dictionary<string, string>
                 {
                     { "client_id", clientId },
                     { "client_secret", clientSecret },
                     { "grant_type", "refresh_token" },
-                    { "refresh_token", refreshToken }
+                    { "refresh_token", tokens.RefreshToken }
                 };
-                HttpFormUrlEncodedContent content = new HttpFormUrlEncodedContent(dict);
-                HttpResponseMessage res = await client.PostAsync(new Uri($"{authorizationUrl}/connect/token"), content);
-                if (res.IsSuccessStatusCode)
-                {
-                    var str = await res.Content.ReadAsStringAsync();
-                    var token = JsonConvert.DeserializeObject<TokenResponse>(str);
-                    localSettings.Values[AccessTokenKey] = token.access_token;
-                    var expiration = DateTime.Now.AddSeconds(token.expires_in);
-                    localSettings.Values[AccessTokenExpiresKey] = expiration.ToString(CultureInfo.InvariantCulture);
-                    localSettings.Values[RefreshTokenKey] = token.refresh_token;
-                    accessToken = token.access_token;
-                    refreshToken = token.refresh_token;
-                    accessTokenExpiration = expiration;
-                    client.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", accessToken);
-                    return client;
+                    HttpFormUrlEncodedContent content = new HttpFormUrlEncodedContent(dict);
+                    HttpResponseMessage res = await client.PostAsync(new Uri($"{authorizationUrl}/connect/token"), content);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        var str = await res.Content.ReadAsStringAsync();
+                        var token = JsonConvert.DeserializeObject<TokenResponse>(str);
+                        tokens = new StoredTokens()
+                        {
+                            AccessToken = token.access_token,
+                            RefreshToken = token.refresh_token,
+                            AccessTokenExpires = DateTime.Now.AddSeconds(token.expires_in)
+                        };
+                        Tokens = tokens;
+                        client.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", tokens.AccessToken);
+                        return client;
+                    }
+                    else
+                    {
+                        throw new UnauthorizedException("Could not redeem refresh token.");
+                    }
                 }
-                else
+                finally
                 {
-                    throw new UnauthorizedException("Could not redeem refresh token.");
+                    accessTokenSem.Release();
                 }
             }
-            throw new UnauthorizedException("No valid access or refresh token configured.");
+            else
+            {
+                accessTokenSem.Release();
+                throw new UnauthorizedException("No valid access or refresh token configured.");
+            }
         }
 
         public async Task AuthenticateClient()
         {
             var state = Guid.NewGuid().ToString();
             var nonce = Guid.NewGuid().ToString();
-            var webAuthenticationResult =
-              await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None,
-              new Uri($"{authorizationUrl}/connect/authorize?client_id={clientId}&scope={scopes} openid offline_access&response_type=code id_token&" +
-              $"redirect_uri={redirectUri}&state={state}&nonce={nonce}"),
-              new Uri(redirectUri));
-
-            if (webAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
+            await accessTokenSem.WaitAsync();
+            try
             {
-                var data = webAuthenticationResult.ResponseData;
-                var parms = new WwwFormUrlDecoder(new Uri(data.Replace("#", "?")).Query);
-                if (state != parms.Where(p => p.Name == "state").Single().Value)
+                var webAuthenticationResult =
+                  await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None,
+                  new Uri($"{authorizationUrl}/connect/authorize?client_id={clientId}&scope={scopes} openid offline_access&response_type=code id_token&" +
+                  $"redirect_uri={redirectUri}&state={state}&nonce={nonce}"),
+                  new Uri(redirectUri));
+
+                if (webAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
                 {
-                    throw new UnauthorizedException("State differs");
-                }
-                var code = parms.Where(p => p.Name == "code").Single().Value;
-                var client = DefaultClient();
-                var dict = new Dictionary<string, string>();
-                dict.Add("client_id", clientId);
-                dict.Add("client_secret", clientSecret);
-                dict.Add("grant_type", "authorization_code");
-                dict.Add("code", code);
-                dict.Add("redirect_uri", redirectUri);
-                HttpFormUrlEncodedContent content = new HttpFormUrlEncodedContent(dict);
-                HttpResponseMessage res = await client.PostAsync(new Uri($"{authorizationUrl}/connect/token"), content);
-                if (res.IsSuccessStatusCode)
-                {
-                    var str = await res.Content.ReadAsStringAsync();
-                    var token = JsonConvert.DeserializeObject<TokenResponse>(str);
-                    localSettings.Values[AccessTokenKey] = token.access_token;
-                    var expiration = DateTime.Now.AddSeconds(token.expires_in);
-                    localSettings.Values[AccessTokenExpiresKey] = expiration.ToString(CultureInfo.InvariantCulture);
-                    localSettings.Values[RefreshTokenKey] = token.refresh_token;
-                    accessToken = token.access_token;
-                    refreshToken = token.refresh_token;
-                    accessTokenExpiration = expiration;
+                    var data = webAuthenticationResult.ResponseData;
+                    var parms = new WwwFormUrlDecoder(new Uri(data.Replace("#", "?")).Query);
+                    if (state != parms.Where(p => p.Name == "state").Single().Value)
+                    {
+                        throw new UnauthorizedException("State differs");
+                    }
+                    var code = parms.Where(p => p.Name == "code").Single().Value;
+                    var client = DefaultClient();
+                    var dict = new Dictionary<string, string>();
+                    dict.Add("client_id", clientId);
+                    dict.Add("client_secret", clientSecret);
+                    dict.Add("grant_type", "authorization_code");
+                    dict.Add("code", code);
+                    dict.Add("redirect_uri", redirectUri);
+                    HttpFormUrlEncodedContent content = new HttpFormUrlEncodedContent(dict);
+                    HttpResponseMessage res = await client.PostAsync(new Uri($"{authorizationUrl}/connect/token"), content);
+                    if (res.IsSuccessStatusCode)
+                    {
+                        var str = await res.Content.ReadAsStringAsync();
+                        var token = JsonConvert.DeserializeObject<TokenResponse>(str);
+                        var tokens = new StoredTokens()
+                        {
+                            AccessToken = token.access_token,
+                            RefreshToken = token.refresh_token,
+                            AccessTokenExpires = DateTime.Now.AddSeconds(token.expires_in)
+                        };
+                        Tokens = tokens;
+                    }
+                    else
+                    {
+                        throw new UnauthorizedException("Could not redeem refresh token.");
+                    }
                 }
                 else
                 {
-                    throw new UnauthorizedException("Could not redeem refresh token.");
+                    throw new UnauthorizedException("Browser authentication was not successful");
                 }
             }
-            else
+            finally
             {
-                throw new UnauthorizedException("Browser authentication was not successful");
+                accessTokenSem.Release();
             }
         }
     }
